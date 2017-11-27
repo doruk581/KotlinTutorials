093608709dc5f3a825ce43b00d141188bc0c7bb1
Doruk Su <doruk.su@semper-tech.com>
2017-11-27 17:13:40 +0300

    new Examples

---
+++ src/CarClass.kt
@@ -0,0 +1,46 @@
+import sun.plugin.liveconnect.OriginNotAllowedException
+
+class Car(Type:String, Model:Int, Price:Double, MilesDrive:Int, Owner:String)
+{
+    var Owner:String?=null
+    var Price:Double?=null
+    var MilesDrive:Int?=null
+    var Model:Int?=null
+    var Type:String?=null
+
+
+    init {
+        println("Type: $Type")
+        println("Model:$Model")
+        println("Price:$Price")
+        println("MilesDrive:$MilesDrive")
+        println("Owner: $Owner")
+
+        this.Owner = Owner
+        this.Price = Price
+        this.MilesDrive = MilesDrive
+        this.Model = Model
+        this.Type = Type
+    }
+
+    fun GetPrice():Double? {
+        return this.Price!! - (this.MilesDrive!!.toDouble() * 10)
+    }
+
+    fun GetOwner():String? {
+        return this.Owner
+    }
+
+
+}
+
+fun main(args:Array<String>)
+{
+    var myCar = Car("BMW",2015,10000.0,105,"Hussein")
+    println("Get Owner:" + myCar.GetOwner())
+    println("Get Price:" + myCar.GetPrice())
+
+    var yourCar = Car("KA",2017,20000.0,1,"Jena")
+    println("Get Owner: " + yourCar.GetOwner())
+    println("Get Price:" + yourCar.GetPrice())
+}
